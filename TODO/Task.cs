a777ec9f96a4b4c4bc2d9c5178427144cc306fee
phillyai <sonagi.philly@gmail.com>
2017-06-11 19:34:08 +0900

    초기 커밋

---
+++ TODO/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TODO
+{
+    [Serializable]
+    public class Task
+    {
+        public DateTime When { get; set; }
+        public string What { get; set; }
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
---
+++ TODO/TaskTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace TODO
+{
+    [Serializable]
+    public class TaskTable
+    {
+        public List<Task> Tasks { get; }
+
+        public TaskTable()
+        {
+            Tasks = new List<Task>();
+        }
+
+        public void SaveToFile(string path)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(TaskTable));
+            using (StreamWriter sw = new StreamWriter(path))
+                xml.Serialize(sw, this);
+        }
+
+        public static TaskTable LoadFromFile(string path)
+        {
+            TaskTable t;
+            XmlSerializer xml = new XmlSerializer(typeof(TaskTable));
+            using (StreamReader sr = new StreamReader(path))
+                t = xml.Deserialize(sr) as TaskTable;
+            return t;
+        }
+    }
+}
