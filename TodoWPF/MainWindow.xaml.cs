2dd545cbd09899c7d46f7fe5eb04e0aa0d7f622a
phillyai <sonagi.philly@gmail.com>
2017-06-11 20:57:24 +0900

    WPF로 한번 프로토타입 만들어봄

---
+++ TodoWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoWPF
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class TaskView
+    {
+        private ObservableCollection<TodoTask> _tasks;
+        public ObservableCollection<TodoTask> TaskCollection => _tasks;
+
+        public TaskView()
+        {
+            _tasks = new ObservableCollection<TodoTask>();
+            _tasks.Add(new TodoTask(DateTime.Now, "10시간 코딩해라"));
+        }
+    }
+}
---
+++ TodoWPF/TodoTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TodoWPF
+{
+    [Serializable]
+    public class TodoTask
+    {
+        public DateTime When { get; set; }
+        public string What { get; set; }
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
+
+        public TodoTask(DateTime when, string what)
+        {
+            Done = false;
+            When = when;
+            What = what;
+        }
+    }
+}
